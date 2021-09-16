e468aa342179b0e048202f9a576255f678ec7246
cristian-sudo <cplop21@gmail.com>
2021-09-16 14:37:44 +0100

    Css

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,13 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./index.css";
 
-
-function Prova(){
+function BookList(){
   return (
-<div>
-  <h1>CIAO</h1>
-</div>
-  );
+<section className="BookList">
+<Book/>
+<Book/>
+<Book/>
+<Book/>
+<Book/>
+<Book/>
 
+</section>
+  );
 }
-ReactDOM.render(<Prova/>, document.getElementById("root"));
+
+const Book = () =>{
+  return (
+    <article className="Book">
+      <Image/>
+      <Title/>
+      <Author/>
+    </article>
+  )
+}
+
+const Image = () =>{
+  return (
+    <img src="https://images-na.ssl-images-amazon.com/images/I/A1ITodlM7JS._AC_UL200_SR200,200_.jpg" alt="" />
+  )
+}
+
+const Title =()=>{
+  return (
+<h1 style={{color:"red", fontSize:"0.75rem", marginTop:"0.25rem"}}>Change Sings: A Children's Anthem</h1>
+  );
+}
+const Author =()=>{
+  return (
+<h4>Amanda Gorman</h4>
+  );
+}
+
+ReactDOM.render(<BookList/>, document.getElementById("root"));
